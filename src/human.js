f7574cb4b83dee1e5be437261099a76ff1e57057
Dillon Titcomb <dillontitcomb@gmail.com>
2018-03-16 11:16:55 -0700

    add preliminary functions with conversion constants as well as front page and form.

--- src/human.js
+++ src/human.js
@@ -1,13 +1,26 @@
-export class Human{
-	constructor(name, dateOfBirth, foodHabits, exerciseHabits) {
+const toDay = 365;
+const toHour = 8765.76;
+const toMin = 525945.6;
+const toSec = 31556736;
+const toMilli = 31556736000;
+
+const daysMercury = 88;
+const daysVenus = 255;
+const daysMars = 686.98;
+const daysJupiter = 4300;
+
+export class Human {
+	constructor(name, dateOfBirth, foodHabits, exerciseHabits, socialHabits) {
 		this.name = name;
 		this.dateOfBirth = dateOfBirth;
 		this.foodHabits = foodHabits;
 		this.exerciseHabits = exerciseHabits;
+		this.socialHabits = socialHabits;
 	};
 
-	// const yearToMilli = 31557600000;
-	// const yearToSec = 315576000;
-	//
-	// dateOfBirthToAge(conversionType)
+	dateOfBirthToAge(conversionType) {
+		let birthday = new Date(this.dateOfBirth);
+		let dateValue = Date.now() - birthday;
+		return dateValue / conversionType;
+	}
 }
